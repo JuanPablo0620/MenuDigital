8074ee49c1fe24daea44035f146386413c95fa14
Juan Pablo <varinijuanpablo@gmail.com>
2025-03-04 20:02:28 -0300

    first commit

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HamburguesasNav from '../ProductosNav/HamburguesasNav'
+
+const Nav = () => {
+  return (
+    <div>
+        <HamburguesasNav />
+    </div>
+  )
+}
+
+export default Nav
