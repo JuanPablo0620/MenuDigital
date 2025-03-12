9553897ed731295193afb314d66231938cee1301
Juan <varinijuanpablo@gmail.com>
2025-03-11 22:48:21 -0300

    apiMock colocado en el /public

--- src/components/BurguerCard/BurguerCard.jsx
+++ src/components/BurguerCard/BurguerCard.jsx
@@ -1,23 +1,22 @@
-import React from 'react'
+import React from 'react';
 
-const BurguerCard = ({burguer}) => {
-
-    const {name, price, description, image} = burguer;
+const BurguerCard = ({ burguer }) => {
+  const { name, price, description, image } = burguer;
 
   return (
     <>
-    <div className='border shadow-md shadow-gray-400 border-gray-200 flex p-3 text-principal font-archivo rounded-2xl'>
+      <div className='border shadow-md shadow-gray-400 border-gray-200 flex p-3 text-principal font-archivo rounded-2xl'>
         <div className='flex flex-col gap-1 w-[130%]'>
-           <h1 className='font-bold'>{name}</h1>
-           <p>{description}</p>
-           <span className='text-red-500 font-bold'>${price}</span> 
+          <h1 className='font-bold'>{name}</h1>
+          <p>{description}</p>
+          <span className='text-red-500 font-bold'>${price}</span>
         </div>
         <div className=''>
-            <img className='rounded-2xl' src={image}/>
+          <img className='rounded-2xl' src={image} alt={name} />
         </div>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default BurguerCard
+export default BurguerCard;
